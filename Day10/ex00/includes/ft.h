4026e06a1534de61f306950bda2fcf3f58be84bf
Gawen Loriann <gloriann@ox-c1.21-school.ru>
2019-08-11 16:50:24 +0300

    day10 ex00

---
+++ Day10/ex00/includes/ft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gloriann <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/08/11 14:13:44 by gloriann          #+#    #+#             */
+/*   Updated: 2019/08/11 16:14:35 by gloriann         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#ifndef FT_H
+//#define FT_H
+
+int		ft_putchar(char c);
+void	ft_swap(int *a, int *b);
+void	ft_putstr(char *str);
+int		ft_strlen(char *str);
+int		ft_strcmp(char *s1, char *s2);
+
+//#endif
