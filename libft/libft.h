ae457c793f41556f861593f40b6343845c537a3e
Dmitry Degtyarev <dimad83@gmail.com>
2019-09-16 15:01:54 +0300

    ft_putchar, ft_putchar_fd, ft_pustr

---
+++ libft/libft.h
@@ -0,0 +1,10 @@
+#ifndef LIBFT_H
+# define LIBFT_H
+# include <string.h>
+
+void ft_putchar(char c);
+void ft_putchar_fd(char c, int fd);
+void ft_putnbr(int n);
+void ft_putstr(char const *s);
+
+#endif
