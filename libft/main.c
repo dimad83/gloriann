7cbeeeab0477f368861b2cf8b943d07017506a30
Dmitry Degtyarev <dimad83@gmail.com>
2019-09-24 14:56:23 +0300

    main.c

--- libft/main.c
+++ libft/main.c
@@ -3,19 +3,10 @@
 #include <stdlib.h>
 #include <string.h>
 
-char	*ft_strnstr(const char *haystack, const char *needle, size_t n);
-char	*strnstr(const char *s, const char *find, size_t slen);
+char	*ft_strtrim(char const *s);
+
 int main(int argc, char **argv)
 {
-	int n;
-
-	if (argc == 4)
-	{
-		n = atoi(argv[3]);
-		printf("strnstr\(\'%s', \'%s\', %d)\n", argv[1], argv[2], n);
-		printf("my  : %s\n", ft_strnstr(argv[1], argv[2], n));
-		printf("orig: %s\n", strnstr(argv[1], argv[2], n));
-		//printf("my  : %s\n", ft_strstr(argv[1], argv[2]));
-		//printf("orig: %s\n", strstr(argv[1], argv[2]));
-	}
+	if (argc == 2)
+		printf("ft_strtrim: '%s' => '%s'\n", argv[1], ft_strtrim(argv[1]));
 }
