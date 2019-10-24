35d9bcc76e5efc910c1330c8de6c728fc5267345
dimad83 <dimad83@gmail.com>
2019-10-24 10:14:23 +0300

    Update get_next_line.c

--- gnl/get_next_line.c
+++ gnl/get_next_line.c
@@ -1,9 +1,22 @@
-//
-// Created by Gawen Loriann on 20/10/2019.
-//
+#include "get_next_line.h"
 
-int get_next_line(const int fd, char **line)
+void    ft_die(char *msg)
 {
-	static char *rest;
+	ft_putstr_fd("Error: ", 2);
+	ft_putendl_fd(msg, 2);
+	return(-1);
+}
+
+int		get_next_line(const int fd, char **line)
+{
+	static char	*s;
+	char	buf[BUFF_SIZE];
+
+	//buf = ft_strnew(BUFF_SIZE);
+	if (fd == NULL)
+		ft_die("No pointer to a file.");
+	readed_size = read(fd, buf, BUFF_SIZE);
+	if (readed_chars < 0)
+		ft_die();
 
 }
